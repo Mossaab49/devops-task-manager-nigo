f811c2a0926eee7b5213c7e9ff101fcbbe0e8066
Nigo <saouti.mossaab@etu.uae.ac.ma>
2026-02-22 13:24:41 +0000

    Refactor app.js on main

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const tasksRouter = require('./routes/tasks');
 
 app.get('/', (req, res) => {
-  res.json({ message: "Welcome from main branch..." });
+  res.json({ message: "Welcome from main branch" });
 });
  
 app.use('/tasks', tasksRouter);
