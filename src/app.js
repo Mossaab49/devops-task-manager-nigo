651c2eee62c07224a130eac66ad3b99ff40e414e
Nigo <saouti.mossaab@etu.uae.ac.ma>
2026-02-22 13:49:26 +0000

    change

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const tasksRouter = require('./routes/tasks');
 
 app.get('/', (req, res) => {
   res.json({ message: "Hey welcome from main branch" });
 });
  
+app.use('/tasks', tasksRouter);
 
 app.listen(3000, ()=> console.log("API running on port 3000"));
